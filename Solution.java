3723d4007f2aaf6a6627e7d24087028958f0c9d5
sdim-ua <ingersol@yandex.ua>
2019-06-14 09:57:19 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,17 @@
+
+/* 
+Заклятые друзья
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class AppleIPhone {
+
+    }
+
+    public class SamsungGalaxyS2 extends AppleIPhone {
+
+    }
+}
